7089d32af5cf172d9b562490ea005a2735be2dd7
will-weiss <will.weiss1230@gmail.com>
2017-02-21 10:48:53 -0500

    simple compiler for prolog

--- src/english_syntax_trees_to_prolog_ast.bash
+++ src/english_syntax_trees_to_prolog_ast.bash
@@ -1,3 +1,31 @@
 english_syntax_trees_to_prolog_ast() {
-  cat
+  # Read and discard lines for now
+  while read line; do
+    :
+  done
+
+  echo '{
+    "type": "program",
+    "clauses": [
+      {
+        "type": "predicate",
+        "name": "format",
+        "arguments": [
+          {
+            "type": "string",
+            "value": "~s~n"
+          },
+          {
+            "type": "list",
+            "contents": [
+              {
+                "type": "string",
+                "value": "Hello World"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }' | jq --compact-output '.'
 }
--- src/prolog_ast_to_prolog_code.bash
+++ src/prolog_ast_to_prolog_code.bash
@@ -1,8 +1,33 @@
 prolog_ast_to_prolog_code() {
-  # Pass for now
-  while read line; do
-    :
-  done
+  node -e '
+    require("readline")
+      .createInterface({ input: process.stdin })
+      .on("line", printPrologCodeForLine)
 
-  echo 'format("~s~n", ["Hello World"]).'
+    function printPrologCodeForLine(line) {
+      const ast = JSON.parse(line)
+      const prologCodeForLine = getPrologCodeOfNode(ast)
+      console.log(prologCodeForLine)
+    }
+
+    function getPrologCodeOfNode(node) {
+      switch (node.type) {
+        case "program": {
+          return node.clauses.map(getPrologCodeOfNode).join(" ")
+        }
+        case "predicate": {
+          return node.name + "(" + node.arguments.map(getPrologCodeOfNode).join(", ") + ")."
+        }
+        case "string": {
+          return "\"" + node.value + "\""
+        }
+        case "list": {
+          return "[" + node.contents.map(getPrologCodeOfNode).join(", ") + "]"
+        }
+        default: {
+          throw new Error("Unrecognized node type: " + node.type)
+        }
+      }
+    }
+  '
 }
